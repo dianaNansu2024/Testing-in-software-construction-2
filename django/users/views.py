182fe293598b8eb49c85f273cb722c6a78987a93
dianaNansu2024 <promise24556@gmail.com>
2025-03-16 23:39:30 +0300

    created the users django apllication

---
+++ django/users/views.py
@@ -0,0 +1,36 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from .serializers import RegisterSerializer, LoginSerializer
+
+class RegisterView(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            "user": {
+                "username": user.username,
+                "email": user.email,
+            },
+            "token": token.key
+        }, status=status.HTTP_201_CREATED)
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            "user": {
+                "username": user.username,
+                "email": user.email,
+            },
+            "token": token.key
+        }, status=status.HTTP_200_OK)
