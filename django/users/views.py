9debc12ad7cd2b58bc01871cde3d9e49d3886368
dianaNansu2024 <promise24556@gmail.com>
2025-03-16 23:49:48 +0300

    ceated the project and user api files

--- django/users/views.py
+++ django/users/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
